ea446d7930be123e6ee7ac9160c585d3f2f679c8
Akhilesh Kumar <skakhilesh0017@gmail.com>
2018-11-04 20:14:09 +0530

    main actiivity

---
+++ app/src/main/java/com/akhilesh002/welcome/MainActivity.java
@@ -0,0 +1,20 @@
+package com.akhilesh002.welcome;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView.setTextColor(Color.BLUE);
+    }
+}
